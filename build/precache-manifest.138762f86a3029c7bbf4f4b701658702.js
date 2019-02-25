07588697fbe12d2547a177a42396278ffb04d881
OStefani <ostefanishina@gmail.com>
2019-02-25 14:07:15 +0300

    server

---
+++ build/precache-manifest.138762f86a3029c7bbf4f4b701658702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "87b91d5fb3d8b54891ff",
+    "url": "/static/js/main.87b91d5f.chunk.js"
+  },
+  {
+    "revision": "1b63493ba778dbfc59f4",
+    "url": "/static/js/2.1b63493b.chunk.js"
+  },
+  {
+    "revision": "87b91d5fb3d8b54891ff",
+    "url": "/static/css/main.83a53c4c.chunk.css"
+  },
+  {
+    "revision": "8a7784b678c87ac4727cefb279e3899f",
+    "url": "/index.html"
+  }
+];
