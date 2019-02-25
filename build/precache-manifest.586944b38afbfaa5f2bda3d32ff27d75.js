557afb38235f4c0fdb4fa53d9beb876475fd3f75
OStefani <ostefanishina@gmail.com>
2019-02-25 15:59:43 +0300

    add build

---
+++ build/precache-manifest.586944b38afbfaa5f2bda3d32ff27d75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8290b53bbab3d49d96b4",
+    "url": "/static/js/main.8290b53b.chunk.js"
+  },
+  {
+    "revision": "1b63493ba778dbfc59f4",
+    "url": "/static/js/2.1b63493b.chunk.js"
+  },
+  {
+    "revision": "8290b53bbab3d49d96b4",
+    "url": "/static/css/main.13b6e7ef.chunk.css"
+  },
+  {
+    "revision": "2091468ce32dfebe2a6a07e463028a5c",
+    "url": "/index.html"
+  }
+];
